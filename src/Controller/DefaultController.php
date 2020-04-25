9cbb56558ee3606bbcd1ebc4919c41a36a18d420
Andrei Dyatlov <fantazey@gmail.com>
2020-04-26 06:43:06 +0700

    + symfony/webpack-encore + index route for serve init html

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/{reactRouting}", name="index", requirements={"reactRouting"="^(?!api).+"}, defaults={"reactRouting": null})
+     */
+    public function index() {
+        return $this->render('index.html.twig');
+    }
+}
